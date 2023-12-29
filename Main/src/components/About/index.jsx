ca503f7c2943a5c0a7690ef79ea789c299b71e1b
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2023-12-29 18:14:21 -0500

    adding text to index.jsx

---
+++ Main/src/components/About/index.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+          <i className="fas fa-user-circle" style={{ fontsize: "96px" }}></i>
+        </div>
+        <p>
+          {" "}
+          Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch
+          Etsy authentic Marfa quinoa typewriter plaid direct trade small batcl
+        </p>
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          quibusdam magni quis amet, fuga nostrum qui, voluptatem, vero
+          repellendus quia a non recusandae perspiciatis! Quasi ratione qui fuga
+          cum labore.
+        </p>
+      </div>
+    </section>
+  );
+}
+export default About;
