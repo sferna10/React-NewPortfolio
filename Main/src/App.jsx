3cab7c44408e656d0b2d68fe05d8f87f2b0f3a8f
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-03 23:03:33 -0500

    adding files and code

---
+++ Main/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Page from "./components/Page";
+import { useLocation } from "react-router-dom";
+
+function App() {
+    const currentPage = useLocation().pathname;
+
+}
