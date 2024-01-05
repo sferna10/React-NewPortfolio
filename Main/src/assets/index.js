6430c55a5d710cb69df693ef03233456495d11af
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-04 23:51:44 -0500

    Debugging

--- Main/src/assets/index.js
+++ Main/src/assets/index.js
@@ -1,7 +1,7 @@
 export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
-export { default as repo1 } from './projects/repo1.jpg';
+export { default as repo2 } from './projects/repo2.jpg';
+export { default as repo3 } from './projects/repo3.jpg';
+export { default as repo4 } from './projects/repo4.jpg';
+export { default as repo5 } from './projects/repo5.jpg';
+export { default as repo6 } from './projects/repo6.jpg';
+
--- Main/src/components/Portfolio/index.jsx
+++ Main/src/components/Portfolio/index.jsx
@@ -7,23 +7,23 @@ const [projects] = useState([
 {
     name: 'Professional Portfolio',
     description: 'HTML and CSS',
-    link: "https://github.com",
-    repo: "https://github.com"
+    link: "https://sferna10.github.io/Portfolio_HTML-CSS/",
+    repo: "https://github.com/sferna10/Portfolio_HTML-CSS"
 },
 {   name: 'Password Generator',
     description: 'JavaScript',
-    link: "https://github.com",
-    repo: "https://github.com"
+    link: "https://github.com/sferna10/Password-Generator",
+    repo: "https://sferna10.github.io/Password-Generator/"
 },
 {   name: 'Quiz_Challenge',
     description: ' dkdfds',
-    link: "https://github.com",
-    repo: "https://github.com"
+    link: "https://github.com/sferna10/Quiz_Challenge",
+    repo: "https://sferna10.github.io/Quiz_Challenge/"
 },
 {   name: 'Work_Day_Scheduler',
     description: 'JavaScript',
-    link: "https://github.com",
-    repo: "https://github.com"
+    link: "https://github.com/sferna10/Work_Day_Scheduler",
+    repo: " https://sferna10.github.io/Work_Day_Scheduler/"
 },
 {   name: 'Employee-Tracker-Database',
     description: 'JavaScript',
@@ -32,15 +32,13 @@ const [projects] = useState([
 },
 {   name: 'Note-Taker-Challenge',
     description: 'JavaScript',
-    link: "https://github.com",
+    link: "https://github.com/sferna10/Note-Taker-Challenge",
     repo: "https://github.com"
 },
 {   name: 'Professiona_README_generator',
     description: 'JavaScript',
-    link: "https://github.com",
-    repo: "https://github.com"
-},
-{   name: 'FinalVersionPortfolio',
+    link: "https://github.com/sferna10/Professional_README_generator",
+    repo: " https://sferna10.github.io/Professional_README_generator/"
     description: 'JavaScript',
     link: "https://github.com",
     repo: "https://github.com"
